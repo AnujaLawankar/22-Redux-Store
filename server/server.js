7c84e60bbdab60a11b9139af195fadbf76b233f8
lawankar.anuja08@gmail.com <lawankar.anuja08@gmail.com>
2023-07-21 18:21:34 -0400

    Deploying on heroku

--- server/server.js
+++ server/server.js
@@ -31,21 +31,9 @@ const startApolloServer = async () => {
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
 
-
-
     app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, '../client/dist/index.html'));
     });
-
-
-
-
-    app.use(express.static("build"));
-    app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "build", "index.html"));
-    });
-
-
   }
 
   db.once('open', () => {
