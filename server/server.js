b843d1fb88dd64041807b09c2d1e1927b0ea39ea
lawankar.anuja08@gmail.com <lawankar.anuja08@gmail.com>
2023-07-21 18:01:56 -0400

    Depolying on heroku

--- server/server.js
+++ server/server.js
@@ -31,9 +31,21 @@ const startApolloServer = async () => {
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
 
+
+
     app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, '../client/dist/index.html'));
     });
+
+
+
+
+    app.use(express.static("build"));
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "build", "index.html"));
+    });
+
+
   }
 
   db.once('open', () => {
